2796c13abf6795b52f191e4998f78078c64c2374
Canchili <148684811+Canchili@users.noreply.github.com>
2024-04-14 23:35:37 +1000

    Update main.c

--- main.c
+++ main.c
@@ -11,17 +11,22 @@
 
   */
 int main(void) {
-  int i, n;
+  int i, n, m = 0;
 
   FILE *f1;
   char m1[100], m2[100], m3[100], m4[100];
   f1 = fopen("all.txt", "r");
   FILE *f2;
   f2 = fopen("young.txt", "w");
-
-  for (i = 0; i < 30; i++) {
+  while (fscanf(f1, "%s", m1) != EOF) {
+    m++;
+  }
+  printf("%d", m);
+  fclose(f1);
+  f1 = fopen("all.txt", "r");
+  for (i = 0; i < m; i++) {
     fgets(m1, 100, f1);
-    printf("%s", m1);
+    //  printf("%s", m1);
     sscanf(m1, "%s %s %s %d", m2, m3, m4, &n);
     if (n > 1980) {
       fprintf(f2, "%s", m1);
